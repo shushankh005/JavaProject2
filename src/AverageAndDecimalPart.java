96a81dd32844d2d8abf065245e8f594f5319d142
Shushan <shushankhachatryan005@gmail.com>
2023-01-06 23:08:26 +0400

    Add Second /AverageAndDecimalPart/ Java Project

--- src/AverageAndDecimalPart.java
+++ src/AverageAndDecimalPart.java
@@ -1,4 +1,4 @@
-public class AvaragAndDecimalPart {
+public class AverageAndDecimalPart {
     public static void main(String[] args) {
         //-----------------------------------------------------
 
