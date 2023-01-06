a390c03b9d1f2c2d3f4611dca50655afcaa477cc
Shushan <shushankhachatryan005@gmail.com>
2023-01-06 23:10:41 +0400

    Add Third /Triangle/ Java Project

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Triangle {
+import java.nio.file.FileSystemNotFoundException;
+import java.util.Scanner;
+
+public class Triangle {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Print 3 values for valid triangle");
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int c = s.nextInt();
+        if ((a + b) > c && (a + c) > b && (b + c) > a) {
+            System.out.println("We will have a valid triangle");
+        } else {
+            System.out.println("We will have an invalid triangle");
+        }
+    }
 }
+
