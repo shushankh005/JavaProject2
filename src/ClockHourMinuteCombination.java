630f0e4e9eb354ba006140e2be12560ebc8c5a60
Shushan <shushankhachatryan005@gmail.com>
2023-01-10 03:25:12 +0400

    Add 6th /ClockHourMinuteCombination/ Java Project

--- src/ClockHourMinuteCombination.java
+++ src/ClockHourMinuteCombination.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class ClockHourMinuteCombination {
+import java.util.Scanner;
+
+public class ClockHourMinuteCombination {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Clock hour-minute combination for 1-12 sections");
+        int d = s.nextInt();
+        if (d >= 1 && d <= 11) {
+            System.out.println("Choosing section " + d + ", the minute hand will show " + d * 5 + "th minute");}
+        else if (d==12) {
+            System.out.println("Choosing section " + d + " , it will be a round hour");}
+        else {
+            System.out.println("Error!");}
+    }
 }
+
