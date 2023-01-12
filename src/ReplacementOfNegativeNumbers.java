9d509e37abacf349d00898fdd7c180d9557fecb2
Shushan <shushankhachatryan005@gmail.com>
2023-01-13 03:12:13 +0400

    Add 8th /ReplacementOfNegativeNumbers/ Java Project

--- src/ReplacementOfNegativeNumbers.java
+++ src/ReplacementOfNegativeNumbers.java
@@ -1,13 +1,20 @@
 public class ReplacementOfNegativeNumbers {
     public static void main(String[] args) {
 
-        int[] arrmix = {12,5,-6,-5,8,9,-7,0,-10,15};
-        int[] arrmod = new int[arrmix.length];
-int i,j=0;
+        int[] arrmix = {12, 5, -6, -5, 8, 98, -77, 0, -10, 15};
+        int n = 0; //The negative numbers initialization
 
-        System.out.println("Array of negative numbers modulo will be ");
-        System.out.println("The number of modulo changes = " );
-        System.out.println("The number of non-negative members of the arrmix = " );
+        System.out.print("Array of negative numbers modulo will be {");
 
+        for (int i = 0; i < arrmix.length; i++) {
+            if (arrmix[i] < 0) {
+                arrmix[i] = -1 * arrmix[i];
+                n++;
+            }
+            System.out.print(arrmix[i] + ",");
+        }
+        System.out.println("}");
+        System.out.println("The number of modulo changes will be " + n);
+        System.out.println("The number of non-negative members of the arrmix will be " + (arrmix.length - n));
     }
 }
