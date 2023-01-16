027908d5825b913b246c656fa598514b0364d350
Shushan <shushankhachatryan005@gmail.com>
2023-01-16 19:45:46 +0400

    Add 12th /IntLargerThanPreviousFromArray/ Java Project

--- src/IntLargerThanPreviousFromArray.java
+++ src/IntLargerThanPreviousFromArray.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class IntLargerThanPreviousFromArray {
+public class IntLargerThanPreviousFromArray {
+    public static void main(String[] args) {
+        int[] arrmix = {8, 11, -6, 52, 61, 99, -77, 0, 100, 15};
+        int k = 0;
+        System.out.print("The quantity of numbers greater than the previous one will be: ");
+        for (int i = 1; i < arrmix.length; i++) {
+            if (arrmix[i] > arrmix[i - 1]) {
+                k++;
+            }
+        }
+        System.out.print(k);
+
+    }
 }
