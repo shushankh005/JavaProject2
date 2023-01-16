a7967f3d47c27df3b8ea68fd5e6627af1cdf862c
Shushan <shushankhachatryan005@gmail.com>
2023-01-16 19:43:38 +0400

    Add 11th /TwoDimensionalArraysDifferentLengths/ Java Project

--- src/TwoDimensionalArraysDifferentLengths.java
+++ src/TwoDimensionalArraysDifferentLengths.java
@@ -1,2 +1,22 @@
 public class TwoDimensionalArraysDifferentLengths {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {5, 12, 44, -8, 7, 25},
+                {85, -3, 9},
+                {21, -7, 2, 11},
+                {4, 66}
+        };
+        int j = 0;
+        System.out.println("The longest array will be ");
+        System.out.print("{");
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i].length > matrix[j].length) {
+                j = i;
+            }
+        }
+        for (int i = 0; i < matrix[j].length; i++) {
+            System.out.print(matrix[j][i] + ",");
+        }
+        System.out.print("}");
+    }
 }
