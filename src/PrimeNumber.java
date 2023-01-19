b9ba4ee3cde7f6f154d91f0122ab4771ff4e967a
Shushan <shushankhachatryan005@gmail.com>
2023-01-20 01:44:41 +0400

    Add 14th /PrimeNumber/ Java Project

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class PrimeNumber {
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int[] array = {8, 2, 555, 7, 1, 27};
+        System.out.print("The Prime Numbers from array are { ");
+
+        for (int i = 0; i < array.length; i++) {
+            boolean isPrime = true;
+            if (array[i] == 1)
+                isPrime = false;
+            else {
+                for (int j = 2; j <= array[i] / 2; j++) {
+                    if (array[i] % j == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+            if (isPrime) {
+                if (array[i] == 0) {
+                } else {
+                    System.out.print(array[i] + ",");
+                }
+            }
+        }
+        System.out.println(" } ");
+    }
 }
